8ecaec0b89b83904065aac24e75837c4e7ed0096
cnadals <carmen.nadal@gmail.com>
2016-09-23 15:25:30 +0200

    Probando commits

--- calcoo.py
+++ calcoo.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+import sys
+
 class Calculadora():
     def suma(self, op1, op2):
         return op1 + op2
@@ -9,6 +11,17 @@ class Calculadora():
         return op1 - op2
 
 if __name__ == "__main__":
-    calculadora = Calculadora()
-    resultado = calculadora.suma(3,5)
+    try:
+        operando1 = int(sys.argv[1])
+        operando2 = int(sys.argv[3])
+    except ValueError:
+        sys.exit("Error: Non numerical parameters")
+
+    if sys.argv[2] == "suma":
+        resultado = plus(operando1, operando2)
+    elif sys.argv[2] == "resta":
+        resultado = minus(operando1, operando2)
+    else:
+        sys.exit('Operación sólo puede ser sumar o restar.')
+
     print(resultado)
