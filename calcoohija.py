a2cf26a4d9fba7594a914e7a1c9befe021006963
cnadals <carmen.nadal@gmail.com>
2016-09-23 17:12:14 +0200

    Creadas clases. Añadida excepcion y heredado. ERROR CalculaDeNuevo = CalculadoraHija() SIN RESOLVER.

---
+++ calcoohija.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+class Calculadora():
+    def suma(self, op1, op2):
+        return op1 + op2
+
+    def resta(self, op1, op2):
+        return op1 - op2
+
+class CalculadoraHija(Calculadora):
+
+    def multiplicacion(self, op1, op2):
+        return op1 * op2
+
+    def division(self, op1, op2):
+        if op2 == 0:
+            return "Division by zero is not allowed"
+        else:
+            return op1 / op2
+
+if __name__ == "__main__":
+
+    try:
+        operando1 = int(sys.argv[1])
+        operando2 = int(sys.argv[3])
+    except ValueError:
+        sys.exit("Error: Non numerical parameters")
+
+	CalculaDeNuevo = CalculadoraHija()
+
+    if sys.argv[2] == "suma":
+        resultado = CalculaDeNuevo.suma(operando1, operando2)
+    elif sys.argv[2] == "resta":
+        resultado = CalculaDeNuevo.resta(operando1, operando2)
+    elif sys.argv[2] == "multiplica":
+        resultado = CalculaDeNuevo.multiplicacion(operando1, operando2)
+    elif sys.argv[2] == "divide":
+        resultado = CalculaDeNuevo.division(operando1, operando2)
+    else:
+        sys.exit('Operación no válida.')
+
+    print(resultado)
